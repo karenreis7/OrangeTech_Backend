60d6cad6288d67d9b0fce3c87cd9e8c9cfa5583c
karenreis7@hotmail.com <karenreis7@hotmail.com>
2023-01-06 17:45:11 -0300

    Adição do novo módulo sobre POO

---
+++ src/POO/Carro.java
@@ -0,0 +1,47 @@
+package POO;
+
+// Exercicio Básico de como criar uma classe e seus atributos//
+public class Carro {
+    String nome;
+    int placa;
+    String modelo;
+    String cor;
+    int capacidadetanque;
+
+    // Construtor abaixo dos atributos (boa pratica de organização)
+
+    Carro(){
+        // Construtor mais simples, vazio, somente com o nome da classe
+    }
+
+    Carro(String cor, String modelo, int capacidadetanque){ //Sobrecarga de método
+        this.cor = cor;
+        this.modelo = modelo;                               /* this é uma forma de diferencia o que pertence ou não ao objeto */
+        this.capacidadetanque = capacidadetanque;
+    }
+
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public int getCapacidadetanque() {
+        return capacidadetanque;
+    }
+
+    public void setCapacidadetanque(int capacidadetanque) {
+        this.capacidadetanque = capacidadetanque;
+    }
+
+    double TotalValorTanque(double valorCombustivel){
+        return capacidadetanque * valorCombustivel;
+    }
+}
---
+++ src/POO/EstruturaBasicaPoo.md
@@ -0,0 +1,10 @@
+# Orange Tech Backend 🍊 
+
+## Estrutuba Básica da POO
+
+### Classe: 
+* É uma estrutura que abstrai um conjunto de objetos com caracteristicas similares.
+* Define o comportamento dos seus objetos através de metódos e os estados possiveis 
+através de seus atributos. 
+* O objetivo da Classe é definir, servindo de base, para o que futuramente será Objeto. 
+* É a forma mais basica de se definir apenas uma vez como deve ser todos os objetos criados a partir dela. 
